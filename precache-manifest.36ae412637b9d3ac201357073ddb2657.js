e326f15e41888a53422c5dfab4524596c263b306
pft\\Ruby_Feng <Ruby_Feng@PerfectCorp.com>
2020-03-31 18:16:53 +0800

    Updates

---
+++ precache-manifest.36ae412637b9d3ac201357073ddb2657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d012439469d4c5f9669495e5fa9759f8",
+    "url": "/cat-animation/index.html"
+  },
+  {
+    "revision": "6b7a42e1467187cab8ad",
+    "url": "/cat-animation/static/css/main.36bf41cb.chunk.css"
+  },
+  {
+    "revision": "416c9ba5b7ef9cce6671",
+    "url": "/cat-animation/static/js/2.8cdf735d.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/cat-animation/static/js/2.8cdf735d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b7a42e1467187cab8ad",
+    "url": "/cat-animation/static/js/main.34bc1d26.chunk.js"
+  },
+  {
+    "revision": "4ec1054cf3a99973e981",
+    "url": "/cat-animation/static/js/runtime-main.2bb32b5d.js"
+  }
+]);
